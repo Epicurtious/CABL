b61d2b1a4875073ad0fd1d31a39899ce3c760528
Curtis Goodwin <curtisgoodwin117@yahoo.com>
2020-02-02 09:47:36 -0500

    Page with Data

--- webpage.py
+++ webpage.py
@@ -1,10 +1,17 @@
+import json
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+with open("actualInfo.json",'r') as j:
+    posts = json.load(j)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
+@app.route("/data")
+def data():
+    return render_template("data.html", posts=posts[0]["listings"], dates=posts[0]["dates"])
 
 if __name__ == "__main__":
     app.run(debug=True)
